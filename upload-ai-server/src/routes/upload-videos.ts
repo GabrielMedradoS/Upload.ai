cf7ac181c083337033f0a7415a16baf359dda6c2
Gabriel Medrado <gabriel.medradoo@hotmail.com>
2023-11-18 20:47:12 -0300

    upload-videos initial commit

---
+++ upload-ai-server/src/routes/upload-videos.ts
@@ -0,0 +1,22 @@
+import { FastifyInstance } from "fastify";
+import { fastifyMultipart } from "@fastify/multipart";
+import { prisma } from "../lib/prisma";
+import path from "node:path";
+
+export async function uploadVideoRoute(app: FastifyInstance) {
+  app.register(fastifyMultipart, {
+    limits: {
+      fileSize: 1_048_576 * 25, //25mb
+    },
+  });
+
+  app.post("/prompt", async (request, reply) => {
+    const data = await request.file();
+
+    if (!data) {
+      return reply.status(400).send({ error: "Missing file input..." });
+    }
+
+    const extension = path.extname(data.filename);
+  });
+}
